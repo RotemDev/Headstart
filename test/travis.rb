283c7753e2003ae430d375edefc41993998d506d
RoteMinecraftIL <rotem.moses@outlook.co.il>
2016-10-16 11:30:49 +0300

    Deleted test/travis.rb

--- test/travis.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-result = `sass scss/rotem.scss css/rotem.css`
-raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('built.css')
-puts "Regular compile worked successfully"
